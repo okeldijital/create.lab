5d3956c8d3857e5231fc35851945b742a735eb64
Nkululeko Nkosi <okeldijital@gmail.com>
2026-08-12 02:57:01 +0200

    feat(build-005): export organization persistence composition

--- packages/composition/src/index.ts
+++ packages/composition/src/index.ts
@@ -6,3 +6,7 @@ export {
 } from "./ApplicationComposition.js";
 export type { ApplicationComposition, ApplicationCompositionOptions } from "./ApplicationComposition.js";
 export { RepositoryRegistry } from "./RepositoryRegistry.js";
+export {
+  ORGANIZATION_REPOSITORY_KEYS,
+  registerPostgresOrganizationRepositories,
+} from "./OrganizationPersistenceComposition.js";
