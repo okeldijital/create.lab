9a74a28af459f1cc65baf8e96fdd17a38a7609f1
Nkululeko Nkosi <okeldijital@gmail.com>
2026-08-12 02:03:43 +0200

    feat(build-003): expose composition boundary

---
+++ packages/composition/src/index.ts
@@ -0,0 +1,8 @@
+export {
+  createApplicationComposition,
+  executeCommand,
+  registerCommandHandler,
+  registerQueryHandler,
+} from "./ApplicationComposition.js";
+export type { ApplicationComposition, ApplicationCompositionOptions } from "./ApplicationComposition.js";
+export { RepositoryRegistry } from "./RepositoryRegistry.js";
