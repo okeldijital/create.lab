7c2f00bb6d9d027b549e57f2618c4112d872b2d0
Nkululeko Nkosi <okeldijital@gmail.com>
2026-08-18 15:22:27 +0200

    feat(auth): expose organization membership repository port

--- packages/organization/src/repositories/index.ts
+++ packages/organization/src/repositories/index.ts
@@ -3,3 +3,4 @@ export type { DepartmentRepository } from "./DepartmentRepository.js";
 export type { TeamRepository } from "./TeamRepository.js";
 export type { StudioRepository } from "./StudioRepository.js";
 export type { OrganizationSettingsRepository } from "./OrganizationSettingsRepository.js";
+export type { OrganizationMembershipRepository } from "./OrganizationMembershipRepository.js";
