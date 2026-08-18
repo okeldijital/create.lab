449997a00b08c0c44e177760bd15c3e0902f9078
Nkululeko Nkosi <okeldijital@gmail.com>
2026-08-18 15:20:41 +0200

    feat(auth): expose authorization implementation

--- packages/application/src/authorization/index.ts
+++ packages/application/src/authorization/index.ts
@@ -1,1 +1,3 @@
 export type { AuthorizationService } from "./AuthorizationService.js";
+export { DefaultAuthorizationService } from "./DefaultAuthorizationService.js";
+export type { MembershipReader } from "./DefaultAuthorizationService.js";
